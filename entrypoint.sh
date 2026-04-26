e39518d68c9dff81654c146f3f335d6d495e5c70
AAKON <faisalakon444@gmail.com>
2026-04-26 22:13:41 +0600

    asdhjksd

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,30 +8,32 @@ CONFIG_FILE="${CONFIG_DIR}/config.json"
 if [ ! -f "${CONFIG_FILE}" ]; then
     mkdir -p "${CONFIG_DIR}"
 
-    PICOCLAW_MODEL="${PICOCLAW_MODEL:-anthropic/claude-sonnet-4-6}"
+    PICOCLAW_MODEL="${PICOCLAW_MODEL:-google/gemini-2.5-flash}"
     TELEGRAM_ENABLED="${TELEGRAM_ENABLED:-false}"
 
     cat > "${CONFIG_FILE}" <<EOF
 {
+  "agents": {
+    "defaults": {
+      "model_name": "default-model"
+    }
+  },
   "gateway": {
     "host": "0.0.0.0",
     "port": 10000
   },
-  "providers": {
-    "openrouter": {
-      "api_key": "${OPENROUTER_API_KEY}"
-    }
-  },
   "model_list": [
-      {
-        "model_name": "default-model",
-        "model": "openrouter/${PICOCLAW_MODEL}"
-      }
-    ],
-  "telegram": {
-    "enabled": ${TELEGRAM_ENABLED},
-    "bot_token": "${TELEGRAM_BOT_TOKEN}",
-    "user_id": "${TELEGRAM_USER_ID}"
+    {
+      "model_name": "default-model",
+      "model": "openrouter/${PICOCLAW_MODEL}"
+    }
+  ],
+  "channels": {
+    "telegram": {
+      "enabled": ${TELEGRAM_ENABLED},
+      "token": "${TELEGRAM_BOT_TOKEN}",
+      "allow_from": ["${TELEGRAM_USER_ID}"]
+    }
   }
 }
 EOF
