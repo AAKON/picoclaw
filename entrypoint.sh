f5b60df2e44bd06923866c7da0c61fece71a5a36
Aakon <faisalakon444@gmail.com>
2026-04-28 10:57:54 +0600

    fix: use gemini-3-flash-preview

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ cat > "${CONFIG_FILE}" <<EOF
   "model_list": [
     {
       "model_name": "default-model",
-      "model": "gemini-3.1-flash-lite-preview",
+      "model": "gemini-3-flash-preview",
       "api_key": "${GEMINI_API_KEY}",
       "api_base": "https://generativelanguage.googleapis.com/v1beta/openai/"
     }
